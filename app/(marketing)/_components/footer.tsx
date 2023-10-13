d101e87572444374f00c4a8fa5f72be0c481aba1
AmirrezaJM <amirr.jolani007@gmail.com>
2023-10-13 22:37:57 +0200

    notion project

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="flex items-center w-full p-6 bg-background z-50">
+            <Logo />
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+    return ( 
+        <div>
+            Hello
+        </div>
+    );
+}
+
+
+export default Logo;
