ba1da9a1dc742ccedc188e604853a40134d4cddd
AmirrezaJM <amirreza.jolani@yahoo.com>
2023-10-17 10:30:19 +0200

    add covex into project.

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 <ModeToggle />
-                
             </div>
         </div>
     );
